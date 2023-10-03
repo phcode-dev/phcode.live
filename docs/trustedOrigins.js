c5c22a2048c4f9d41a1ed3b50db4b3e3314e826e
Arun Bose <tellarunbose@gmail.com>
2023-10-03 10:07:13 +0530

    Update trusted origins

--- docs/trustedOrigins.js
+++ docs/trustedOrigins.js
@@ -1,5 +1,11 @@
 export const TRUSTED_ORIGINS = {
     'http://localhost:8000': true, // phcode dev server
+    'http://localhost:8001': true, // phcode dev live preview server
+    'http://localhost:5000': true, // playwright tests
+    'http://127.0.0.1:8000': true, // phcode dev server
+    'http://127.0.0.1:8001': true, // phcode dev live preview server
+    'http://127.0.0.1:5000': true, // playwright tests
+    'https://phcode.live': true, // phcode prod live preview server
     'https://phcode.dev': true,
     'https://dev.phcode.dev': true,
     'https://staging.phcode.dev': true,
